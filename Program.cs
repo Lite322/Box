1430ca521caa05ac6206d08820f5669291e960d9
EvanPlay <99335865+EvanPlay@users.noreply.github.com>
2022-03-09 08:31:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,50 @@
-﻿using System;
-    namespace Box
+using System;
+namespace Box
 {
     class MainClass
     {
-        public static void Main(string[]args)
+        public static void Main(string[] args)
         {
-        string s1 = "Я пограмма для выведения на экран квадратов из нулей и единиц, причем нули находятся только на диагоналях квадрата."
-        string s2 = "Пожалуйста, введите сторону квадарата"
-        string[] values = new string[] { s1, s2 };
-        string s3 = string.Join(" ", values);
-        Console.WriteLine(s3);
-            int size = Console.Read();
-            for (int x = 0; x < size; x++)
+            Console.WriteLine("Я программа для выведения на экран квадрата из нулей и единиц, при чём нули назодятся только на диагоналях квадрата.")
+int a = Convert.Int32(Console.ReadLine());
+            int b = Convert.Int32(Console.ReadLine());
+            int c = 0;
+            int d = c;
+            int[,] square = new int[a, b];
+            for (c = 0; c < a; c++)
             {
-                for (int y = 0; y < size; y++)
-                    if (x == y || x == size - y)
-                    { Console.Write("0"); }
+                for (d = 0; d < b; d++)
+                {
+                    if (c == d)
+                    {
+                        square[c, d] = 0;
+                    }
                     else
-                    { Console.Write("1"); }
-                Console.WriteLine();
+                    {
+                        square[c, d] = 1;
+                    }
+                }
             }
-            Console.ReadKey();
+            c--;
+            d--;
+            if (c == a * 1 && d == b * 1)
+            {
+                d = 0;
+                while (c >= 0 && d < b)
+                {
+                    square[c, d] = 0;
+                    c--;
+                    d++;
+                }
+            }
+            for (c = 0; c < a; c++)
+            {
+                for (b = 0; b < b; b++)
+                {
+                    Console.Writeline(square[c, j] + " ");
+                }
+                Console.Writeline("\n")
+            }
         }
+    }
+}
